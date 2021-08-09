c105ab89366c7e1c4413c5ca469800214af7bcda
Nikolai Makarevich <nikoladze529@mail.ru>
2021-08-09 21:36:16 +0300

    feat: add speed control

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const muteSVG = 'url("assets/video/mute.svg")';
 const videoProgress = document.querySelector('#video-progress');
 const volumeProgress = document.querySelector('#volume-progress')
 const videoProgressText = document.querySelector('.video-progress-text');
+const speedControl = document.querySelector('.speed-control');
+const speed = speedControl.querySelector('.speed');
 let videoDuration;
+let videoLengthMinutes;
+let videoLengthSeconds;
+let videoFullLength;
 let currentTime;
 let timer;
 
@@ -16,9 +21,12 @@ volumeProgress.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0
 
 video.addEventListener('loadedmetadata', () => {
   videoDuration = video.duration;
-  currentTime = video.currentTime
+  currentTime = video.currentTime;
+  videoLengthMinutes = Math.floor(videoDuration / 60) < 10 ? `0${Math.floor(videoDuration / 60)}` : Math.floor(videoDuration / 60);
+  videoLengthSeconds = Math.floor(videoDuration % 60) < 10 ? `0${Math.floor(videoDuration % 60)}` : Math.floor(videoDuration % 60);
+  videoFullLength = `${videoLengthMinutes}:${videoLengthSeconds}`
   videoProgress.setAttribute("max", videoDuration)
-  videoProgressText.textContent = `${Math.floor(currentTime / 60)}:${Math.floor(currentTime % 60)}/${Math.floor(videoDuration / 60)}:${Math.floor(videoDuration % 60)}`;
+  videoProgressText.textContent = `00:00/${videoFullLength}`;
 })
 
 playButton.addEventListener('click', videoControl);
@@ -49,13 +57,15 @@ function updateCurrentTime() {
   currentTime = video.currentTime;
   videoProgress.value = currentTime;
   let progressPercent = video.currentTime / videoDuration * 100;
+  let currentMinutes = Math.floor(currentTime / 60) < 10 ? `0${Math.floor(currentTime / 60)}` : Math.floor(currentTime / 60);
+  let currentSeconds = Math.floor(currentTime % 60) < 10 ? `0${Math.floor(currentTime % 60)}` : Math.floor(currentTime % 60);
   videoProgress.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${progressPercent}%, #fff ${progressPercent}%, white 100%)`
-  videoProgressText.textContent = `${Math.floor(currentTime / 60)}:${Math.floor(currentTime % 60)}/${Math.floor(videoDuration / 60)}:${Math.floor(videoDuration % 60)}`;
+  videoProgressText.textContent = `${currentMinutes}:${currentSeconds}/${videoFullLength}`;
   if (video.currentTime == video.duration) {
     playButton.classList.toggle('playing');
     playButton.style.backgroundImage = playSVG;
     videoProgress.value = 0;
-    videoProgressText.textContent = `0:0/${Math.floor(videoDuration / 60)}:${Math.floor(videoDuration % 60)}`;
+    videoProgressText.textContent = `0:0/${MvideoFullLength}`;
     videoProgress.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${0}%, #fff ${0}%, white 100%)`
     removeInterval(timer)
   }
@@ -84,3 +94,11 @@ function volumeControl() {
     video.muted = true;
   }
 }
+
+speedControl.addEventListener('pointermove', (event) => {
+  const value = event.pageY - speedControl.offsetTop;
+  const step = speedControl.offsetHeight / 9;
+  let playbackRate = 0.25 * (10 - Math.round(value / step));
+  speed.style.height = `${(1- playbackRate / 2.5) * 100}%`;
+  video.playbackRate = playbackRate;
+})
